3eddee4d1dfc65b4898687c6346cc5b478d8b437
Ilya Baturin <ivbaturin@gmail.com>
2017-06-23 19:19:10 +0300

    :star: first simple working version
    Simple "input-to-output" file processing

--- main/main.go
+++ main/main.go
@@ -2,37 +2,48 @@ package main
 
 import (
 	"fmt"
+	"github.com/ejilay/skabelon"
+	"io/ioutil"
 	"os"
+	"path"
 )
 
 func main() {
+
 	var (
-		inFile  *os.File
-		outFile *os.File
-		err     error
+		inFile, outFile *os.File
+		err             error
 	)
-	if len(os.Args) != 3 && len(os.Args) != 1 {
+
+	if len(os.Args) != 3 {
 		printUsage()
 		os.Exit(1)
 	}
-	if len(os.Args) == 1 {
-		inFile = os.Stdin
-		outFile = os.Stdout
-	} else {
-		inFile, err = os.Open(os.Args[1])
-		if os.IsNotExist(err) || os.IsPermission(err) {
-			fmt.Println(err)
-			os.Exit(2)
-		}
-		defer inFile.Close()
-		outFile, err = os.Create(os.Args[1])
-		if os.IsNotExist(err) || os.IsPermission(err) {
-			fmt.Println(err)
-			os.Exit(3)
-		}
+	inFile, err = os.Open(os.Args[1])
+	if os.IsPermission(err) || os.IsNotExist(err) {
+		exitWithCode(err, 2)
 	}
-	defer outFile.Close()
 
+	outFile, err = os.Create(os.Args[2])
+	if os.IsPermission(err) || os.IsNotExist(err) {
+		exitWithCode(err, 3)
+	}
+
+	tmplBuf, err := ioutil.ReadAll(inFile)
+	if err != nil {
+		exitWithCode(err, 4)
+	}
+	skabelon := skabelon.New()
+	if err = skabelon.Parse(path.Base(os.Args[1]), string(tmplBuf)); err != nil {
+		exitWithCode(err, 5)
+	}
+	if err = skabelon.Exec(outFile); err != nil {
+		exitWithCode(err, 6)
+	}
+}
+func exitWithCode(err error, code int) {
+	fmt.Println(err)
+	os.Exit(code)
 }
 
 func printUsage() {
--- skabelon.go
+++ skabelon.go
@@ -1,16 +1,62 @@
+/*
+Package skabelon implements total templating functionality
+*/
 package skabelon
 
-type Skabelon struct {
-	*skabelonInternals
-}
+import (
+	"os"
+	"strings"
+	"text/template"
+)
 
-type skabelonInternals struct {
-	inited      bool
-	environmets map[string]string
+type Skabelon struct {
+	values   map[string]string
+	options  []string
+	template *template.Template
 }
 
 func New() *Skabelon {
-	skabelonInstance := new(Skabelon)
-	skabelonInstance.skabelonInternals = new(skabelonInternals)
-	return skabelonInstance
+	instance := new(Skabelon)
+	instance.options = []string{"missingkey=error"}
+	instance.initSysEnv()
+	return instance
+}
+
+func (instance *Skabelon) SetOptions(options []string) {
+	instance.options = options
+}
+
+func (instance *Skabelon) initSysEnv() {
+	instance.values = make(map[string]string)
+	for _, tuple := range os.Environ() {
+		pair := strings.Split(tuple, "=")
+		instance.Set(pair[0], pair[1])
+	}
+}
+
+func (instance *Skabelon) Set(key, value string) {
+	instance.values[key] = value
+}
+
+func (instance *Skabelon) Get(key string) (string, bool) {
+	val, exist := instance.values[key]
+	return val, exist
+}
+
+func (instance *Skabelon) Parse(name, tmpl string) error {
+	if instance.template == nil {
+		instance.template = template.New(name).Option(instance.options...)
+	}
+	var tt *template.Template
+	if instance.template.Name() == name {
+		tt = instance.template
+	} else {
+		tt = instance.template.New(name)
+	}
+	_, err := tt.Parse(tmpl)
+	return err
+}
+
+func (instance *Skabelon) Exec(file *os.File) error {
+	return instance.template.Execute(file, instance.values)
 }
